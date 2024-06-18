d32c52527d4220e6b4cf0fe44ebef398475adbb9
Oleksandr Horbatiuk <oleksandr.v.horbatiuk@gmail.com>
2024-06-18 14:21:22 +0300

    Issue #1: Implement copying without threads

--- hw01.py
+++ hw01.py
@@ -1,34 +1,47 @@
-from logging import INFO, basicConfig, error, info
+from logging import DEBUG, basicConfig, debug, error, info
 from pathlib import Path
+from shutil import copy
 from sys import argv
 
 
-def scan(folder: Path) -> None:
-    for item in folder.iterdir():
+def scan(source: Path, target: Path) -> None:
+    for item in source.iterdir():
         if item.is_dir():
-            scan(item)
+            scan(item, target)
         else:
-            info(item)
+            sub_target = target / item.suffix[1:] / item.name
+
+            if sub_target.exists():
+                debug(f'Skip file "{item.absolute()}".')
+            else:
+                debug(f'Copying file "{item.absolute()}" to folder '
+                      f'"{sub_target.absolute()}"...')
+
+                sub_target.mkdir(parents=True, exist_ok=True)
+                copy(item, sub_target)
 
 
 def main() -> None:
-    basicConfig(format='%(levelname)s: %(message)s', level=INFO)
+    basicConfig(format='%(levelname)s: %(message)s', level=DEBUG)
 
     if len(argv) < 2:
-        error('Specify source folder as first argument of script.')
+        error('Specify the source folder as the first argument of the script.')
     else:
-        path = Path(source := argv[1])
+        source = Path(argv[1])
 
-        if path.exists():
-            if path.is_dir():
-                target = argv[2] if len(argv) > 2 else 'dist'
+        if source.exists():
+            if source.is_dir():
+                target = Path(argv[2] if len(argv) > 2 else 'dist')
 
-                info(f'Copy files from folder "{path.absolute().name}" to '
-                     f'"{target}".')
+                if target.exists() and not target.is_dir():
+                    error(f'The target "{target}" is not a folder.')
+                else:
+                    info(f'Copying files from folder "{source.absolute()}" to '
+                         f'"{target.absolute()}"...')
 
-                scan(path)
+                    scan(source, target)
             else:
-                error(f'The source "{source}" is not folder.')
+                error(f'The source "{source}" is not a folder.')
         else:
             error(f'The source folder "{source}" not found.')
 
