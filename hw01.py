7d0cc678d1ce79f567a83ef90ac77eb9b31b0530
Oleksandr Horbatiuk <oleksandr.v.horbatiuk@gmail.com>
2024-06-18 16:33:33 +0300

    Issue #1: Use a separate thread for each action, such as scanning folders and copying files

--- hw01.py
+++ hw01.py
@@ -1,28 +1,56 @@
-from logging import DEBUG, basicConfig, debug, error, info
+from abc import ABC, abstractmethod
+from logging import INFO, basicConfig, error, info, warning
 from pathlib import Path
 from shutil import copy
 from sys import argv
+from threading import Semaphore, Thread
 
 
-def scan(source: Path, target: Path) -> None:
-    for item in source.iterdir():
-        if item.is_dir():
-            scan(item, target)
-        else:
-            sub_target = target / item.suffix[1:] / item.name
+class Worker(ABC, Thread):
+    _args: list[Path]
 
-            if sub_target.exists():
-                debug(f'Skip file "{item.absolute()}".')
+    def __init__(self,
+                 source: Path,
+                 target: Path,
+                 semaphore: Semaphore) -> None:
+        super().__init__(name=f'{self.__class__.__name__} of {source}',
+                         args=(source, target))
+        self._semaphore = semaphore
+        self.start()
+
+    @abstractmethod
+    def _action(self) -> None:
+        ...
+
+    def run(self) -> None:
+        with self._semaphore:
+            self._action()
+
+
+class Copyist(Worker):
+    def _action(self) -> None:
+        info(f'Copying the file to folder "{self._args[1]}"...')
+        self._args[1].mkdir(parents=True, exist_ok=True)
+        copy(*self._args)
+
+
+class Scanner(Worker):
+    def _action(self) -> None:
+        for source in self._args[0].iterdir():
+            if source.is_dir():
+                Scanner(source, self._args[1], self._semaphore)
             else:
-                debug(f'Copying file "{item.absolute()}" to folder '
-                      f'"{sub_target.absolute()}"...')
+                target = self._args[1] / source.suffix[1:] / source.name
 
-                sub_target.mkdir(parents=True, exist_ok=True)
-                copy(item, sub_target)
+                if target.exists():
+                    warning(f'Skip file "{source.name}".')
+                else:
+                    Copyist(source, target.parent, self._semaphore)
 
 
 def main() -> None:
-    basicConfig(format='%(levelname)s: %(message)s', level=DEBUG)
+    basicConfig(format='%(threadName)s\n%(levelname)s: %(message)s\n',
+                level=INFO)
 
     if len(argv) < 2:
         error('Specify the source folder as the first argument of the script.')
@@ -36,10 +64,10 @@ def main() -> None:
                 if target.exists() and not target.is_dir():
                     error(f'The target "{target}" is not a folder.')
                 else:
-                    info(f'Copying files from folder "{source.absolute()}" to '
-                         f'"{target.absolute()}"...')
+                    info(f'Copying files from folder "{source}" to '
+                         f'"{target}"...')
 
-                    scan(source, target)
+                    Scanner(source, target, Semaphore(10))
             else:
                 error(f'The source "{source}" is not a folder.')
         else:
